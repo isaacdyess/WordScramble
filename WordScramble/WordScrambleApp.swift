408811b0385334172741c47403214af69a7c2178
Isaac <43579566+isaacdyess@users.noreply.github.com>
2023-06-29 17:31:59 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Isaac Dyess on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
